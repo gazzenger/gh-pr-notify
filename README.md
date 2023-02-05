8962182a157afd4475ed364a7f20e53626737840
Gary Namestnik <gazzenger@gmail.com>
2023-02-05 17:06:33 +0800

    Add script

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # GH-PR-NOTIFY
 
-This repository is for notifying a user when their PRs change status from reviewers, as well as the user being assigned any PRs.
+This repository is for notifying a user when their Github PRs change status from reviewers, as well as the user being assigned any PRs.
 
 Notification is via the notify-send command, as well email?
+
+## Prerequisites
+* libnotify (https://wiki.archlinux.org/title/Desktop_notifications)
+* github-cli (https://github.com/cli/cli)
+* postfix (for emailing)
+
+Setup the script to run in cron
+i.e.
+```bash
+# Note: "1000" would be your user id, the output of... "id -u <username>"
+* * * * * cd ~/gh-pr-notify && DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus ./run.sh
+```
+
+
---
+++ run.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#PAGER=""
+
+#LOG_FILE=./output.log
+LAST_VALUE_FILE=./last-value
+
+# grab last minute
+latest_minute=$(date +"%Y-%m-%dT%H:%M:00%:z")
+
+# grab any previous value from file
+if [ -e $LAST_VALUE_FILE ]; then
+    prev_value=$(< $LAST_VALUE_FILE )
+else
+    # generate the previous minute
+    prev_value=$(date +"%Y-%m-%dT%H:%M:00%:z" --date '-1 month')    
+fi
+
+
+# Output latest_minute value to file for reading next run
+echo $latest_minute > $LAST_VALUE_FILE
+
+# standard filters
+STATE_FILTER=closed
+DRAFT_FILTER=false
+
+#printf 'Checking from %s to %s\n' $prev_value $latest_minute >> $LOG_FILE
+
+# PRs with change_request
+change_request_urls=$(gh search prs --state=$STATE_FILTER --draft=$DRAFT_FILTER --author=@me --review changes_requested --updated="$prev_value..$latest_minute" --json url --jq ".[].url")
+
+# PRs with approval
+approved_urls=$(gh search prs --state=$STATE_FILTER --draft=$DRAFT_FILTER --author=@me --review approved --updated="$prev_value..$latest_minute" --json url --jq ".[].url")
+
+# PRs you've been assigned to
+assigned_urls=$(gh search prs --state=$STATE_FILTER --draft=$DRAFT_FILTER --review-requested=@me --updated="$prev_value..$latest_minute" --json url --jq ".[].url")
+
+
+for item in ${change_request_urls//\\n/ } ; do
+    notify-send "PR has been requested to have changes made" "$item" -u critical --icon=edit-undo
+done
+
+for item in ${approved_urls//\\n/ } ; do
+    notify-send "PR has been approved" "$item" -u critical --icon=emblem-default
+done
+
+for item in ${assigned_urls//\\n/ } ; do
+    notify-send "PR has been assigned to you for review" "$item" -u critical --icon=task-due
+done
+
